8c09199dcb34890fdfdc99bb45152f619779f552
OOOrion <46031480+OOOrion@users.noreply.github.com>
2019-01-05 16:31:02 +0200

    Delete cgi_local_webserver.py

--- cgi_local_webserver.py
+++
@@ -1,7 +0,0 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
-
-HOST = ''               
-PORT = 8000             
-
-print('=== Local webserver ===')
-HTTPServer((HOST, PORT), CGIHTTPRequestHandler).serve_forever()
