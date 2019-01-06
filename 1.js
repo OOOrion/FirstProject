281074fe1ee6137e47f0dfa2efd831c61b3bdc9a
OOOrion <46031480+OOOrion@users.noreply.github.com>
2019-01-06 12:41:20 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,9 @@
 var canvas = document.getElementById('c1');
 var ctx = canvas.getContext('2d');
 
-ctx.fillRect(200, 100, 200, 100);
+canvas.onmousemove = function(event){
+	var x = event.offSetX;
+  var y = event.offSetY;
+  ctx.fillRect(x-5, y-5, 10, 10);
+  ctx.fillStyle = 'green';
+}
